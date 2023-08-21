b68395fd75f3b22929c3de0dd91d80a85c1a9faa
Дмитрий Снитко <snitkody@gmail.com>
2023-08-21 18:49:03 +0300

    selection sort

---
+++ 2-selection-sort.js
@@ -0,0 +1,29 @@
+/**
+ * Selection sort
+ * [1,2,3,4,6,9,7,14]
+ * -sorted----  ?
+ * after sorted part lookup for minimum
+ * place the next minimum number
+ */
+
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minVal = arr[i];
+    let minIdx = i;
+    for(let j = i+1; j < arr.length; j++) {
+      if(minVal > arr[j]) {
+        minVal = arr[j];
+        minIdx = j;
+      }
+    }
+    if(i !== minIdx) {
+      arr[minIdx] = arr[i];
+      arr[i] = minVal;
+    }
+  }
+
+  return arr;
+}
+
+console.log(selectionSort([1,2,3,4,6,9,7,14]));
