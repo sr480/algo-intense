516f267bab930afd09aa6a2279d220564c53dbcb
Дмитрий Снитко <snitkody@gmail.com>
2023-08-21 21:29:23 +0300

    perm

--- 10-permutations.js
+++ 10-permutations.js
@@ -9,3 +9,21 @@
  * backtracking
  * 
  */
+
+
+function permutate(limit, nums = new Set()) {
+  if (nums.size === limit) {
+    return [[...nums.keys()]];
+  }
+  const result = [];
+  for (let i = 1; i <= limit; i++) {
+    if (!nums.has(i)) {
+      const nextNums = new Set(nums);
+      nextNums.add(i);
+      result.push(permutate(limit, nextNums));
+    }
+  }
+  return result.flat(1);
+}
+
+console.log(permutate(3));
