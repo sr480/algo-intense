b60ff587bfb190534bc8903dfd68d99a5d17c051
Дмитрий Снитко <snitkody@gmail.com>
2023-08-21 21:12:09 +0300

    reverse linked list

--- 9-reverse-linked-list.js
+++ 9-reverse-linked-list.js
@@ -1,3 +1,19 @@
 /**
  * Reverse linked list
  */
+
+const data = { val: 1, next: { val: 2, next: { val: 3, next: null } } };
+
+function reverse(start) {
+  let current = start;
+  let prev = null;
+  while(current) {
+    let next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+}
+
+console.log(reverse(data));
